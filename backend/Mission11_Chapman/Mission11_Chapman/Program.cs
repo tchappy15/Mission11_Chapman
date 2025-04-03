d9f8aba75ae8dff38e6137e3848a84cb2e7fc834
tchappy15 <153124713+tchappy15@users.noreply.github.com>
2025-04-03 16:42:44 -0600

    added to cors on program.cs

--- backend/Mission11_Chapman/Mission11_Chapman/Program.cs
+++ backend/Mission11_Chapman/Mission11_Chapman/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "")
+            policy.WithOrigins("http://localhost:3000", "https://wonderful-coast-0fbf3191e.6.azurestaticapps.net")
                 .AllowCredentials() //so that cookies will get added
                 .AllowAnyHeader()
                 .AllowAnyMethod();
